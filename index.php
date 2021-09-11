703bc6c905d34308e3cfcb0cd3f0b9293cabd32f
marcofloriano <marcofloriano@gmail.com>
2021-09-11 14:28:57 -0300

    Vimeo SDK configurado

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'vendor/autoload.php';
+use Vimeo\Vimeo;
+
+$client = new Vimeo("bc003f6ae6fb5e4e508207d93d9852f192e225b1", "OS11rIHsRvBUP33E6VRB5hAXfdfHuvJUxsQIv/F9H/IqKk4s8lROAfx0cs2uffpf9RGEdEJI/zh4w1V7Cj3lU9ZIVvXeOjmOAMOXMIH6In4d5J/HIu6mXzZTePrrNZVd", "ba5f75402548d8aa7d0adddacefd07cd");
+
+$uri = "/videos/489049170"; /// /videos/489049170
+$client->request($uri, array(
+  'name' => '41001 - Curso de WP-CLI - Introdução',
+  'description' => 'Apresentação do Curso de WordPress CLI, a linha de comando do WordPress',
+), 'PATCH');
+
+echo 'The title and description for ' . $uri . ' has been edited.';
