b86d3c4833d1166b7caf7041131fb0ed19c77a3c
marcofloriano <marcofloriano@gmail.com>
2021-09-18 07:11:41 -0300

    Obter todos os videos da Cursos7

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ use Vimeo\Vimeo;
 
 $client = new Vimeo("bc003f6ae6fb5e4e508207d93d9852f192e225b1", "OS11rIHsRvBUP33E6VRB5hAXfdfHuvJUxsQIv/F9H/IqKk4s8lROAfx0cs2uffpf9RGEdEJI/zh4w1V7Cj3lU9ZIVvXeOjmOAMOXMIH6In4d5J/HIu6mXzZTePrrNZVd", "ba5f75402548d8aa7d0adddacefd07cd");
 
+/* Atualizar nome e descrição do vídeo */
+/*
 $uri = "/videos/489049170"; /// /videos/489049170
 $client->request($uri, array(
   'name' => '41001 - Curso de WP-CLI - Introdução',
@@ -11,3 +13,12 @@ $client->request($uri, array(
 ), 'PATCH');
 
 echo 'The title and description for ' . $uri . ' has been edited.';
+*/
+
+/* Obter todos os vídeos da Cursos7 */
+$uri = "/users/8871249/videos";
+$videos = $client->request($uri, array(), 'GET');
+
+echo "<pre>";
+print_r($videos);
+echo "</pre>";
